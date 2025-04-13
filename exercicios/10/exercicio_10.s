37c8f0c6dfc22aa758987560300815e8aadba195
kaiquesouzasantoss <kaiquesouzasantos90@gmail.com>
2025-04-13 15:06:33 -0300

    feat: finalize exercises of first list

--- exercicios/10/exercicio_10.s
+++ exercicios/10/exercicio_10.s
@@ -1,4 +1,66 @@
 .data
+    requisita_a: .asciiz "DIGITE O VALOR DE A: "
+    requisita_b: .asciiz "DIGITE O VALOR DE B: "
+    exibe_resto: .asciiz "RESTO RESULTANTE: "
 .text
-main:
-    
+    # $t0 - A
+    # $t1 - B
+    # $t2 - maior
+    # $t3 - menor
+    # $t4 - divisao
+    # $t5 - resto
+
+    main:
+        # PRINT(requisita_a)
+        li $v0, 4
+        la $a0, requisita_a
+        syscall
+
+        # INPUT
+        li $v0, 5
+        syscall
+
+        # a
+        add $t0, $v0, 0
+
+        # PRINT(requisita_b)
+        li $v0, 4
+        la $a0, requisita_b
+        syscall
+
+        # INPUT
+        li $v0, 5
+        syscall
+
+        # b
+        add $t1, $v0, 0
+
+        # SE($t0 >= $t1)
+        bge $t0, $t1, maior_a
+
+        # SENAO
+        j maior_b
+    maior_a:
+        add $t2, $t0, 0
+        add $t3, $t1, 0
+
+        j divide
+    maior_b:
+        add $t2, $t1, 0
+        add $t3, $t0, 0
+
+        j divide
+    divide:
+        div $t4, $t2, $t3
+        mul $t4, $t4, $t3
+        sub $t5, $t2, $t4
+
+        j finaliza
+    finaliza:
+        li $v0, 4
+        la $a0, exibe_resto
+        syscall
+
+        li $v0, 1
+        add $a0, $t5, 0
+        syscall
--- exercicios/12/exercicio_12.s
+++ exercicios/12/exercicio_12.s
@@ -1,4 +1,97 @@
 .data
-.text
-main:
-    
+    requisita_altura: .asciiz "DIGITE A ALTURA DE UMA PESSOA (CM): " 
+    exibe_maior: .asciiz "MAIOR PESSOA: "
+    exibe_menor: .asciiz "MENOR PESSOA: "
+    unidade: .ascii " CM"
+    quebra_linha: .ascii "\n"
+.text 
+    # $t0 - contador
+    # $t1 - maior
+    # $t2 - menor
+    # $t3 - leitura 
+
+    main:
+        li $t0, 1
+
+        j run
+    run:    
+        # ENQUANTO($t0 <= 15)
+        ble $t0, 15, realiza_leitura
+
+        # SENAO
+        j finaliza
+    realiza_leitura:
+        # PRINT(requisita_altura)
+        li $v0, 4
+        la $a0, requisita_altura
+        syscall
+
+        # INPUT
+        li $v0, 5
+        syscall
+
+        # leitura
+        add $t3, $v0, 0
+
+        # SE($t0 == 1)
+        beq $t0, 1, inicializa_extremos
+
+        # SENAO
+        j verifica_maior
+    verifica_maior:
+        bgt $t3, $t1, modifica_maior
+
+        j incrementa
+    verifica_menor:
+        blt $t3, $t2, modifica_menor
+
+        j incrementa
+    modifica_maior:
+        add $t1, $t3, 0
+
+        j incrementa
+    modifica_menor:
+        add $t2, $t3, 0
+
+        j incrementa
+    inicializa_extremos:
+        add $t1, $t3, 0
+        add $t2, $t3, 0
+
+        j incrementa
+    incrementa:
+        # contador++
+        add $t0, $t0, 1
+
+        j run
+    finaliza:
+        # PRINT(menor)
+        li $v0, 4
+        la $a0, exibe_menor
+        syscall
+
+        li $v0, 1
+        add $a0, $t2, 0
+        syscall
+
+        li $v0, 4
+        la $a0, unidade
+        syscall
+
+        # PRINT(quebra_linha)
+        li $v0, 4
+        la $a0, quebra_linha
+        syscall
+
+        # PRINT(menor)
+        li $v0, 4
+        la $a0, exibe_maior
+        syscall
+
+        li $v0, 1
+        add $a0, $t1, 0
+        syscall
+
+        li $v0, 4
+        la $a0, unidade
+        syscall
